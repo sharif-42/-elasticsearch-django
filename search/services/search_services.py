02e863e9b55993849f88671700ad6419ac2bdee1
sharif_42 <sharifulcsehstu@gmail.com>
2021-07-18 09:57:53 +0600

    book search api enhanced

--- search/services/search_services.py
+++ search/services/search_services.py
@@ -1,10 +1,13 @@
+from elasticsearch_dsl import Q
 from ..documents import AuthorDocument, BookDocument
 
 
 class SearchService:
 
     def get_books(self, **kwargs):
-        books = BookDocument.search().query("match", title="never")
+        keyword = kwargs.get('keyword')
+        query = Q("multi_match", query=keyword, fields=['title', 'isbn'])
+        books = BookDocument.search().query(query)
         return books
 
     def get_authors(self, **kwargs):
